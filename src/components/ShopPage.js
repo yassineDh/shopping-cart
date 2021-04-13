bbbf2b502b02cd7bfdc7863eaa8c69029b475a5e
yassineDh <yassine.dahane96@gmail.com>
2021-04-13 18:55:35 +0000

    add prices and quantity

--- src/components/ShopPage.js
+++ src/components/ShopPage.js
@@ -5,7 +5,14 @@ import { useDispatch, useSelector } from "react-redux";
 function ShopPage() {
   const dispatch = useDispatch();
   const cartProductsData = useSelector((state) => state.cartProducts);
-  let money = cartProductsData.map(el => el.price).reduce((a,b) => a+b);
+
+  let money = cartProductsData
+    .map((el) => el.price * el.quantity)
+    .reduce((a, b) => a + b,0);
+
+  let quantity = cartProductsData
+    .map((el) => el.quantity)
+    .reduce((a, b) => a + b,0);
   return (
     <div>
       <div className="shopping-cart">
@@ -13,14 +20,14 @@ function ShopPage() {
           <CartProduct product={cartProduct} key={cartProduct.id} />
         ))}
       </div>
-      {cartProductsData.length > 0 && 
-      <div className="container">
-        
-        <div className="row">
-          <div>total of orders :</div>
-          <div>amount : {money} </div>
+      {cartProductsData.length > 0 && (
+        <div className="container">
+          <div className="row">
+            <div className="col-3">total of orders : {quantity}</div>
+            <div className="col-3">amount : {money} </div>
+          </div>
         </div>
-        </div>}
+      )}
     </div>
   );
 }
